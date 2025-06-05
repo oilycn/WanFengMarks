7a26c2ae97b7bb7b5f9adfe8c36b415f06cef071
七七 <nijgnoix@gmail.com>
2025-06-05 15:38:09 +0000

    I'll add @next/bundle-analyzer as a development dependency to your packa

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 
 import type {NextConfig} from 'next';
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
 const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
@@ -24,4 +29,4 @@ const nextConfig: NextConfig = {
   // regarding proxy configurations for Server Actions and HMR have been removed as per your request.
 };
 
-export default nextConfig;
+export default withBundleAnalyzer(nextConfig);
