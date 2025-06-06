03cd9645d46fc5b7c46a5e40710a808fffa57675
七七 <nijgnoix@gmail.com>
2025-06-06 04:09:44 +0000

    这样配置网页请求就是304

--- next.config.ts
+++ next.config.ts
@@ -27,16 +27,10 @@ const nextConfig: NextConfig = {
   },
   experimental: {
     serverActions: {
-      // This tells Next.js to trust Server Action requests
-      // if the Origin header is exactly 'https://bm.oily.cn:7443'.
       allowedOrigins: ['https://bm.oily.cn:7443'],
-      // This tells Next.js to trust the X-Forwarded-Host header
-      // if its value is 'bm.oily.cn'.
       allowedForwardedHosts: ['bm.oily.cn'],
     },
   },
-  // This is for the development server HMR and other dev-time communications.
-  // It should match the Origin from which the browser accesses the dev server.
   allowedDevOrigins: ['https://bm.oily.cn:7443'],
 };
 
